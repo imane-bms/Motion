8dd2e2c5f394d3cd50c9f414de2f9da5d4bc9f1f
vskumov <v.kumov@gmail.com>
2024-03-05 13:32:54 +0100

    Masonry added to PostsList

--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 
 export const PostsContainer = styled.div`
 
-  display: grid;
+/* display: flex;
+gap: 2rem;
+
+width: 100%; */
+
+  /* display: grid;
   gap: 1rem; 
   grid-auto-flow: dense; 
   grid-template-columns: repeat(1, 1fr); 
@@ -10,14 +15,14 @@ export const PostsContainer = styled.div`
   @media (min-width: 768px) {
     grid-template-columns: repeat(2, 1fr); 
     gap: 1.5rem;
-  }
+  } */
 
   /* @media (min-width: 1024px) {
     grid-template-columns: repeat(3, 1fr); 
   } */
 
-  @media (min-width: 1281px) {
+  /* @media (min-width: 1281px) {
     grid-template-columns: repeat(3, 1fr);
     gap: 2rem;
-  }
+  } */
 `;
--- src/routes/Layout/index.jsx
+++ src/routes/Layout/index.jsx
@@ -2,7 +2,7 @@
 // import Header from "../../components/Header";
 // import Footer from "../../components/Footer";
 // import SignIn from "../AuthPage/SignIn";
-// import Feed from "../Feed";
+import Feed from "../Feed";
 import Header from "../../components/Header";
 //import NotFound from "../NotFound";
 
@@ -12,7 +12,9 @@ function Layout() {
   return (
     <>
       <Header />
-      <main>{/* <Main /> */}</main>
+      <main>
+        <Feed />
+      </main>
       {/* <Footer /> */}
     </>
   );
