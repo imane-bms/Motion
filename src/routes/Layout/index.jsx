5ee614251c0db10e1444205efa73dec931494b2e
mihael-softic <mihael.softic@gmail.com>
2024-03-05 11:17:32 +0100

    Not Found Page

--- src/routes/Layout/index.jsx
+++ src/routes/Layout/index.jsx
@@ -4,7 +4,7 @@
 // import SignIn from "../AuthPage/SignIn";
 // import Feed from "../Feed";
 import Header from "../../components/Header";
-import NotFound from "../NotFound";
+//import NotFound from "../NotFound";
 
 // import UserPage from "../UserPage";
 
@@ -12,9 +12,7 @@ function Layout() {
   return (
     <>
       <Header />
-      <main>
-        <NotFound />
-      </main>
+      <main>{/* <Main /> */}</main>
       {/* <Footer /> */}
     </>
   );
