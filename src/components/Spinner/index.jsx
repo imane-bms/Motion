f26feaa62d71ac2fd0932f722403e0b3e47aeb14
Erzsébet Erdei <sejlanashop@gmail.com>
2024-03-06 15:22:50 +0000

    Update index.jsx

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -1,6 +1,33 @@
+import styled, { keyframes } from 'styled-components';
+import { RightRoot } from "../../routes/AuthPage/styles";
+
+// Define a keyframe animation for the spinner rotation
+const spinAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+
+const StyledSpinner = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 3px solid #ccc;
+  border-top-color: #333;
+  border-radius: 50%;
+  animation: ${spinAnimation} 1s linear infinite;
+`;
+
 const Spinner = () => {
-    return (
-<div className="spiner">
- <h1>Spinner</h1>
-</div>) };
-export default Spinner
+  return (
+    <RightRoot>
+      <StyledSpinner />
+    </RightRoot>
+  );
+};
+
+export default Spinner;
