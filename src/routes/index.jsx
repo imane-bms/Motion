015f01eac1e1dcf3aa77c09e2d9d9968084031e4
sejlana <sejlanashop@gmail.com>
2024-03-06 17:24:19 +0100

    Found the problem, in the routes/index.jsx file

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./AuthPage";
 import Feed from "./Feed";
 import ProtectedRoutes from "./ProtectedRoutes";
+import NotFound from "./NotFound";
 
 
-import UserEditPage from "../components/UserEditPage";
+//import UserEditPage from "../components/UserEditPage";
 import UserProfilePage from "../components/UserProfilePage";
 
 const Router = () => {
@@ -16,8 +17,7 @@ const Router = () => {
         <Route element={<ProtectedRoutes />}>
         <Route path="/feed" element={<Feed />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/logout" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} /> 
         <Route path="/user/me" element={<UserProfilePage />} />
       </Routes>
     </BrowserRouter>
